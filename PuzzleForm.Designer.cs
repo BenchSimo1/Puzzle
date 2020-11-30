2bbb275313859c2f6209ecb6f842182cc92d70bc
Mohammed Benchelbikh <mohammed.benchelbikh@usmba.ac.ma>
2020-11-30 22:44:36 +0100

    First commit

---
+++ PuzzleForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Puzzle
+{
+    partial class PuzzleForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupCells = new System.Windows.Forms.GroupBox();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.labelBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelBindingSource
+            // 
+            this.labelBindingSource.DataSource = typeof(System.Windows.Forms.Label);
+            // 
+            // groupCells
+            // 
+            this.groupCells.Location = new System.Drawing.Point(37, 35);
+            this.groupCells.Name = "groupCells";
+            this.groupCells.Size = new System.Drawing.Size(387, 328);
+            this.groupCells.TabIndex = 9;
+            this.groupCells.TabStop = false;
+            this.groupCells.Text = "puzzleBox";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(430, 188);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 49);
+            this.timerLabel.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 900;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PuzzleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.groupCells);
+            this.Name = "PuzzleForm";
+            this.Text = "Puzzle";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PuzzleForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.labelBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource labelBindingSource;
+        private System.Windows.Forms.GroupBox groupCells;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PuzzleForm.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class PuzzleForm : Form
+    {
+        Label emptyCell ;
+        Stopwatch timer = new Stopwatch();
+
+        const int NUMBERCOLUMN = 3;
+        const int NUMBERROW = 3;
+
+        Size cellSize = new Size(78 , 78);
+        
+        Point startPointLocation = new Point(30, 26);
+        
+
+        public PuzzleForm()
+        {
+            InitializeComponent();
+            createCells();
+            emptyCell = groupCells.Controls[8] as Label;
+
+
+            Shuffle();
+
+
+        }
+
+        public void Shuffle()
+        {
+            Random rnd = new Random();
+            int n = groupCells.Controls.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                var value = groupCells.Controls[k].Location;
+                groupCells.Controls[k].Location = groupCells.Controls[n].Location;
+                groupCells.Controls[n].Location = value;
+            }
+
+
+        }
+
+        public bool checkWin()
+        {
+            for (int i = 0; i < NUMBERCOLUMN; ++i)
+            {
+                for (int j = 0; j < NUMBERROW; ++j)
+                {
+                    var currentCell = groupCells.Controls.Find($"Cell({i} , {j})", false)[0];
+                    if ( !currentCell.Location.Equals(new Point(startPointLocation.X + cellSize.Width * j, startPointLocation.Y + cellSize.Height * i))) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        }
+
+        void createCells()
+        {
+            
+            for (int i = 0; i < NUMBERCOLUMN; ++i)
+            {
+                for (int j = 0; j < NUMBERROW; ++j)
+                {
+                    Label cell = new Label();
+                    cell.AutoSize = false;
+                    cell.BorderStyle = BorderStyle.Fixed3D;
+                    cell.Font = new Font("Microsoft Sans Serif", 22.2F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+                    cell.Size = cellSize;
+                    cell.Location = new Point(startPointLocation.X + cellSize.Width * j, startPointLocation.Y + cellSize.Height * i);
+                    cell.Name = $"Cell({i} , {j})";
+                    cell.TextAlign = ContentAlignment.MiddleCenter;
+                    cell.TabIndex = i == 0 ? j + 1 : i == 1 ? j + 4 : j + 7;
+
+                    if (i == NUMBERROW - 1 && j == NUMBERCOLUMN - 1)
+                    {
+                        cell.Text = "";
+                    }
+                    else
+                    {
+                        cell.Text = cell.TabIndex.ToString();
+                    }
+                    groupCells.Controls.Add(cell);
+                }
+
+            }
+
+        }
+        
+        void swapEmpty(Label cell)
+        {
+            var value = emptyCell.Location;
+            emptyCell.Location = cell.Location;
+            cell.Location = value;
+
+        }
+
+        Label findLeft()
+        {
+            foreach (Label cell in groupCells.Controls)
+            {
+                if (cell.Location.Y == emptyCell.Location.Y && cell.Location.X == emptyCell.Location.X + cellSize.Width )
+                    return cell;
+            }
+
+            return null;
+        }
+        void moveLeft()
+        {
+            if (emptyCell.Location.X == startPointLocation.X + cellSize.Width * (NUMBERROW -1))
+                return;
+
+            swapEmpty(findLeft());
+        }
+
+
+        Label findRight()
+        {
+            foreach (Label cell in groupCells.Controls)
+            {
+                if (cell.Location.Y == emptyCell.Location.Y && cell.Location.X + cellSize.Width == emptyCell.Location.X)
+                    return cell;
+            }
+
+            return null;
+        }
+        void moveRight()
+        {
+            if (emptyCell.Location.X == startPointLocation.X )
+                return;
+
+            swapEmpty(findRight());
+
+        }
+
+
+        Label findUp()
+        {
+            foreach (Label cell in groupCells.Controls)
+            {
+                if (cell.Location.X == emptyCell.Location.X && cell.Location.Y - cellSize.Height == emptyCell.Location.Y )
+                    return cell;
+            }
+
+            return null;
+        }
+        void moveUp()
+        {
+            if (emptyCell.Location.Y == startPointLocation.Y + cellSize.Height * (NUMBERCOLUMN - 1))
+                return;
+
+            swapEmpty(findUp());
+
+        }
+
+
+        Label findDown()
+        {
+            foreach (Label cell in groupCells.Controls)
+            {
+                if (cell.Location.X == emptyCell.Location.X && cell.Location.Y + cellSize.Height == emptyCell.Location.Y)
+                    return cell;
+            }
+
+            return null;
+        }
+        void moveDown()
+        {
+            if (emptyCell.Location.Y == startPointLocation.Y)
+                return;
+
+            swapEmpty(findDown());
+
+        }
+
+        private void PuzzleForm_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (timerLabel.Text == "")
+            {
+                timer.Start();
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    {
+                        moveLeft();
+                    }
+                    break;
+
+                case Keys.Right:
+                    {
+                        moveRight();
+                       
+                    }
+                    break;
+
+                case Keys.Up:
+                    {
+                        moveUp();
+                    }
+                    break;
+
+                case Keys.Down:
+                    {
+                        moveDown();
+
+                    }
+                    break;
+
+                default:
+                    Console.Beep();
+                    break;
+            }
+
+            if (checkWin())
+            {
+                timer.Stop();
+                MessageBox.Show("WIIN !!");
+                Shuffle();
+                timer.Reset();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (timer.Elapsed.ToString() != "00:00:00")
+                timerLabel.Text = timer.Elapsed.ToString().Remove(8);
+            
+
+            if (timer.Elapsed.Minutes.ToString() == "2")
+            {
+                timer.Reset();
+                timerLabel.Text = "";
+
+                MessageBox.Show("Game over ! loser");
+                Shuffle();
+            }
+
+        }
+    }
+}

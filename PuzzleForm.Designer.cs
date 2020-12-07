e3d8096d4dc7be615ce352d62a58c925bb48327f
Mohammed Benchelbikh <mohammed.benchelbikh@usmba.ac.ma>
2020-12-07 12:37:59 +0100

    not completed yet

--- PuzzleForm.Designer.cs
+++ PuzzleForm.Designer.cs
@@ -65,7 +65,7 @@ namespace Puzzle
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 900;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // PuzzleForm
